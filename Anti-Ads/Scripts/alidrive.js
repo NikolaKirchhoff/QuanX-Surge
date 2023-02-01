f389f47c576cdfc3453ecd61a2dfe1602725097b
NikolaKirchhoff <mx6040@gmail.com>
2023-02-01 16:20:27 +0800

    update to 20230131

--- Anti-Ads/Scripts/alidrive.js
+++
@@ -1,9 +0,0 @@
-/*
-Author:ddgksf2013
-*/
-let obj = JSON.parse($response.body);
-if (obj.result) 
-	{
-		obj.result = Object.values(obj.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
-	}
-$done({ body: JSON.stringify(obj) });
